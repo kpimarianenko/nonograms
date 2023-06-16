b463dac0456a723b7e4b8c97e947237c94aae45c
kpimarianenko <kpimarianenko@gmail.com>
2023-06-16 22:05:03 +0300

    setup apollo client and environment variables

--- client/App.tsx
+++ client/App.tsx
@@ -1,15 +1,26 @@
 import 'react-native-gesture-handler';
 
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
 import AppStackNavigator from '@navigation/AppStackNavigator';
 import NavigationContainer from '@navigation/NavigationContainer';
 
+import config from '@constants/config';
+
 import { I18Provider } from '@i18n';
+
+const client = new ApolloClient({
+  uri: config.apiHost,
+  cache: new InMemoryCache()
+});
 
 const App = () => (
   <I18Provider>
-    <NavigationContainer>
-      <AppStackNavigator isAuthorized={false} />
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <AppStackNavigator isAuthorized={false} />
+      </NavigationContainer>
+    </ApolloProvider>
   </I18Provider>
 );
 
---
+++ client/src/constants/config.ts
@@ -0,0 +1,5 @@
+import Config from 'react-native-config';
+
+export default {
+  apiHost: Config.API_HOST
+};
