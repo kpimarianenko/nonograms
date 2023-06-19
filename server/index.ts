5606889858c09d51f207a5b7de7b840845098b94
kpimarianenko <kpimarianenko@gmail.com>
2023-06-19 19:48:03 +0300

    setup express server and return schema on GET request by root URL

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,13 @@
 import 'reflect-metadata';
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import dotenv from 'dotenv';
+import express from 'express';
+import fs from 'fs';
+import http from 'http';
 import { buildSchema } from 'type-graphql';
 
 import { connectDB } from '@db/connection';
@@ -10,18 +16,37 @@ import UsersResolver from '@resolvers/users.resolver';
 
 dotenv.config();
 
+const app = express();
+const httpServer = http.createServer(app);
+
 const startApolloServer = async () => {
   const schema = await buildSchema({
     resolvers: [UsersResolver],
     emitSchemaFile: true
   });
 
-  const server = new ApolloServer({ schema });
-  const { url } = await startStandaloneServer(server);
+  const server = new ApolloServer({
+    schema,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
+  });
 
-  // eslint-disable-next-line no-console
-  console.log(`Server ready at ${url}`);
+  await server.start();
+
+  app.use(cors(), bodyParser.json(), expressMiddleware(server));
+  await new Promise<void>(resolve => {
+    httpServer.listen({ port: process.env.PORT }, resolve);
+
+    // eslint-disable-next-line no-console
+    console.log(`Server ready at ${process.env.APOLLO_GUI_URL}`);
+  });
 };
+
+app.get('/', (_, res) => {
+  fs.readFile(`${__dirname}/schema.gql`, 'utf8', (err, data) => {
+    if (err) throw err;
+    res.send(JSON.stringify(data));
+  });
+});
 
 startApolloServer();
 connectDB();
